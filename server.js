8da7eb115e16db245ddeba1ed4d1b65137105630
khaizan <khaizanbagood24@gmail.com>
2023-07-12 11:07:24 +0800

    b

--- server.js
+++ server.js
@@ -16,11 +16,11 @@ app.use(bodyParser.urlencoded({ limit: "10mb", extended: true }));
 
     if(process.env.NODE_ENV === 'production')
     {
-        app.use('/' , express.static('/build'))
+        app.use('/' , express.static('client/build'))
 
         app.get('*' , (req,res)=>{
 
-            res.sendFile(path.resolve(__dirname , '/build/index.html'))
+            res.sendFile(path.resolve(__dirname , 'https://kodecaps.onrender.com'))
 
         })
 
